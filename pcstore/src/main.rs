a0cf525e595223caabadfc1b8dbe35fd81ffb8c2
petersantoso94 <m10515809@mail.ntust.edu.tw>
2021-03-17 08:34:34 +0000

    Move gitignore inside

---
+++ pcstore/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+    println!("Key: {} Value: {}", key,value);
+}
